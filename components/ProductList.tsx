407ddaa5378280a4574f703bd4876db86b7bf3ed
anguyen0501 <20h1120185@ut.edu.vn>
2024-06-06 10:00:28 +0700

    feat: fix style productList

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -7,7 +7,7 @@ const ProductList = async () => {
     <div className="flex flex-col items-center gap-10 py-8 px-5">
         <p className="text-heading1-bold">Product</p>
         {!products || products.length === 0 ? (<p className="text-body-bold">No products found</p>) : (
-            <div className="flex flex-wrap mx-auto gap-4">
+            <div className="flex flex-wrap justify-center gap-16">
                 {products.map((product: ProductType) => (
                     <ProductCard key={product._id} product={product}/>
                 ))}
